187bbc474d649d6c98928cb735e899abbba0fb2a
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-09-08 15:57:43 +0200

    Add models

---
+++ app/Models/Idea.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\IdeaStatus;
+use Database\Factories\IdeaFactory;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Idea extends Model
+{
+    /** @use HasFactory<IdeaFactory> */
+    use HasFactory;
+
+    protected $attributes = [
+        'status' => IdeaStatus::PENDING,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function steps(): HasMany
+    {
+        return $this->hasMany(Step::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'links' => AsArrayObject::class,
+            'status' => IdeaStatus::class,
+        ];
+    }
+}
